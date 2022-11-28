a7af96824d015be9cc3692ebf03380b706866c5c
CrazyMint <1938721841@qq.com>
2022-11-28 18:29:12 -0500

    setup env package & implement main screens

---
+++ src/components/Navigation/AppNavigator.tsx
@@ -0,0 +1,39 @@
+import {
+	createStackNavigator,
+	TransitionPresets,
+} from '@react-navigation/stack';
+import { NavigatorScreenParams } from '@react-navigation/native';
+import { AppRouteName } from '../../constants/navigation';
+import { Upload } from '../../screens/Upload';
+import React from 'react';
+import {
+	AppHomeNavigator,
+	AppHomeNavigatorParamList,
+} from './AppHomeNavigator';
+
+export type AppNavigatorParamList = {
+	[AppRouteName.HomeNavigator]: NavigatorScreenParams<AppHomeNavigatorParamList>;
+	[AppRouteName.UploadScreen]: undefined;
+};
+
+const { Navigator, Screen } = createStackNavigator<AppNavigatorParamList>();
+
+export const AppNavigator: React.FC = () => {
+	return (
+		<Navigator
+			initialRouteName={AppRouteName.HomeNavigator}
+			screenOptions={{
+				headerShown: false,
+			}}
+		>
+			<Screen name={AppRouteName.HomeNavigator} component={AppHomeNavigator} />
+			<Screen
+				name={AppRouteName.UploadScreen}
+				component={Upload}
+				options={TransitionPresets.ModalSlideFromBottomIOS}
+			/>
+		</Navigator>
+	);
+};
+
+export default AppNavigator;
